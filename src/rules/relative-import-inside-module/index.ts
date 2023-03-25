e7b5fb9b49d3d4a44857cfb9ce283d67ecf54e0d
Pedro Garcia <drogpe@gmail.com>
2023-03-25 18:51:11 -0300

    write actual rule

--- src/rules/relative-import-inside-module/index.ts
+++ src/rules/relative-import-inside-module/index.ts
@@ -1,5 +1,7 @@
 import { TSESTree } from "@typescript-eslint/utils";
+import ts from "typescript";
 import { createRule } from "../../utils/create-rule";
+import { provider } from "../../utils/provider";
 
 export const relativeImportInsideModule = createRule({
   name: "relative-import-inside-module",
@@ -10,7 +12,12 @@ export const relativeImportInsideModule = createRule({
       requiresTypeChecking: false,
     },
     messages: {
-      // message: "message",
+      insideModuleImportShouldBeRelative:
+        "When importing from a file inside the same module, the import must be relative",
+      outsideModuleImportShouldBeAbsolute:
+        "When importing from another module, the import must be absolute",
+      unableToResolveImport:
+        "The plugin was unable to resolve this import. Ensure it is valid or check your configuration",
     },
     schema: [],
     // hasSuggestions: false,
@@ -20,7 +27,49 @@ export const relativeImportInsideModule = createRule({
   create(context) {
     return {
       ImportDeclaration(node: TSESTree.ImportDeclaration): void {
-        console.log(context.getFilename());
+        provider.initialize(context);
+
+        const currentFile = context.getFilename();
+
+        const { resolvedModule } = ts.resolveModuleName(
+          node.source.value,
+          currentFile,
+          context.parserServices?.program.getCompilerOptions()!,
+          ts.sys
+        );
+
+        if (!resolvedModule?.resolvedFileName)
+          return context.report({
+            node: node,
+            messageId: "unableToResolveImport",
+          });
+
+        const currentFileModule = provider.findModuleOfFile(currentFile);
+        const importedFileModule = provider.findModuleOfFile(
+          resolvedModule.resolvedFileName
+        );
+
+        const isRelativeImport = !!node.source.value.match(/^\.\.?\//);
+
+        const isAbsoluteInsideModule =
+          currentFileModule === importedFileModule && !isRelativeImport;
+
+        if (isAbsoluteInsideModule) {
+          return context.report({
+            node: node,
+            messageId: "insideModuleImportShouldBeRelative",
+          });
+        }
+
+        const isRelativeOutsideModule =
+          currentFileModule !== importedFileModule && isRelativeImport;
+
+        if (isRelativeOutsideModule) {
+          return context.report({
+            node: node,
+            messageId: "outsideModuleImportShouldBeAbsolute",
+          });
+        }
       },
     };
   },
