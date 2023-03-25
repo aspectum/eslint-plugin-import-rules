1fbae2bce3bf6a200522527dabc462ef0010a11e
Pedro Garcia <drogpe@gmail.com>
2023-03-25 18:51:24 -0300

    configure eslint in example

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  parserOptions: { project: ["./tsconfig.json"] },
+  parserOptions: { project: [__dirname + "/tsconfig.json"] },
   plugins: ["@aspectum/import-rules"],
   rules: {
     "@aspectum/import-rules/relative-import-inside-module": "error",
   },
   ignorePatterns: [".eslintrc.js"],
+  settings: {
+    importRules: {
+      modules: ["lib"],
+    },
+  },
 };
