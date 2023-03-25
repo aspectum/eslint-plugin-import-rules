eebdaf9cf883dbdaf6c6b345d2a47f57b8ac463f
Pedro Garcia <drogpe@gmail.com>
2023-03-25 18:15:24 -0300

    add ImportRulesPluginProvider

---
+++ src/types/context-settings.d.ts
@@ -0,0 +1,3 @@
+export interface ImportRules {
+  modules: string[];
+}
---
+++ src/utils/provider/index.ts
@@ -0,0 +1,37 @@
+import type { RuleContext } from "@typescript-eslint/utils/dist/ts-eslint";
+import path from "path";
+import { globSync } from "glob";
+import { ImportRules } from "../../types/context-settings";
+
+class ImportRulesPluginProvider {
+  initialized = false;
+
+  modules: string[] = [];
+
+  initialize(context: Readonly<RuleContext<never, never>>) {
+    if (this.initialized) return;
+
+    (context.settings.importRules as ImportRules).modules.forEach((module) => {
+      let absModule: string;
+      if (path.isAbsolute(module)) {
+        absModule = module;
+      } else {
+        const baseDir =
+          context.parserServices?.program.getCompilerOptions().baseUrl ||
+          path.dirname(
+            context.parserServices?.program
+              .getCompilerOptions()
+              .configFilePath?.toString()!
+          );
+
+        absModule = path.join(baseDir, module);
+      }
+
+      const resolvedModules = globSync(absModule);
+
+      this.modules.push(...resolvedModules);
+    });
+  }
+}
+
+export const provider = new ImportRulesPluginProvider();
