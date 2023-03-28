432859a5c94c7301b6809fc900872d78b437591b
Pedro Garcia <drogpe@gmail.com>
2023-03-27 23:18:41 -0300

    use ts parser to find original files

--- src/rules/imports-in-modules/index.ts
+++ src/rules/imports-in-modules/index.ts
@@ -2,6 +2,7 @@ import { TSESTree } from "@typescript-eslint/utils";
 import ts from "typescript";
 import { createRule } from "../../utils/create-rule";
 import { provider } from "../../utils/provider";
+import { NameAndFile } from "./types";
 
 export const importsInModules = createRule({
   name: "imports-in-modules",
@@ -52,15 +53,48 @@ export const importsInModules = createRule({
         const isAbsoluteInsideModule =
           currentFileModule === importedFileModule && !isRelativeImport;
 
+        const isRelativeOutsideModule =
+          currentFileModule !== importedFileModule && isRelativeImport;
+
+        if (!isAbsoluteInsideModule && !isRelativeOutsideModule) return;
+
+        const typeChecker = context.parserServices?.program.getTypeChecker()!;
+        const tsNode = context.parserServices?.esTreeNodeToTSNodeMap.get(node);
+
+        const def = tsNode?.importClause?.name;
+        const named = tsNode?.importClause?.namedBindings;
+        if (named?.kind === ts.SyntaxKind.NamespaceImport) return;
+
+        // imported objects
+        const indentifiers = [
+          def,
+          ...(named?.elements?.map((specifier) => specifier.name) ?? []),
+        ].filter(Boolean) as ts.Identifier[];
+
+        const importMap = indentifiers.reduce((_importMap, identifier) => {
+          const symbol = typeChecker.getSymbolAtLocation(identifier);
+          if (!symbol) return _importMap;
+          const originalSymbol = typeChecker.getAliasedSymbol(symbol);
+          const fileName = originalSymbol
+            .getDeclarations()?.[0]
+            .getSourceFile().fileName;
+          if (!fileName) return _importMap;
+          _importMap.push({
+            name: identifier.getText(),
+            file: fileName,
+            isDefault: identifier.parent.kind === ts.SyntaxKind.ImportClause,
+          });
+          return _importMap;
+        }, [] as NameAndFile[]);
+
+        console.log(importMap);
+
         if (isAbsoluteInsideModule) {
           return context.report({
             node: node,
             messageId: "insideModuleImportShouldBeRelative",
           });
         }
-
-        const isRelativeOutsideModule =
-          currentFileModule !== importedFileModule && isRelativeImport;
 
         if (isRelativeOutsideModule) {
           return context.report({
---
+++ src/rules/imports-in-modules/types.d.ts
@@ -0,0 +1,5 @@
+export interface NameAndFile {
+  name: string;
+  file: string;
+  isDefault: boolean;
+}
